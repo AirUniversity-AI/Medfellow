62cf8a3fa253213e0e46a80aa524e952592d273c
AirUniversity-AI <221005@students.au.edu.pk>
2025-09-19 08:44:31 +0500

    Delete lib directory

--- lib/__init__.py
+++
@@ -1,8 +0,0 @@
-from .database import get_db_connection, execute_query
-from .board_explainer import GenericBoardStyleMedicalExplainer
-
-__all__ = [
-    'get_db_connection',
-    'execute_query', 
-    'GenericBoardStyleMedicalExplainer'
-]
