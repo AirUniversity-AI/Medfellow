5d1c5c148da3835860b1073fe7f4c8e492b13851
AirUniversity-AI <221005@students.au.edu.pk>
2025-09-19 08:29:13 +0500

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-# app.py - Vercel entry point
-from main import app
-
-# This is the WSGI application that Vercel will use
-# Vercel expects the Flask app to be available as 'app'
-
-if __name__ == "__main__":
-    app.run(debug=False)
